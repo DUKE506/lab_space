ac30c052b3960fb2e8a332dea241edd0a470607b
DUKE506 <dukebs1128@gmail.com>
2026-01-18 22:49:59 +0900

    feat : login 서버 통신 테스트

--- components/features/auth/login-form.tsx
+++ components/features/auth/login-form.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import { Input } from "@/components/ui/input";
 import { useAuthStore } from "@/store/auth-store";
 import { LoginDto } from "@/types/user";
@@ -11,34 +11,34 @@ import { zodResolver } from "@hookform/resolvers/zod";
 const LoginSchema = z.object({
   email: z.string().email(),
   password: z.string().min(6),
-})
+});
 
 export default function LoginForm() {
   const { register, handleSubmit } = useForm<LoginDto>({
     resolver: zodResolver(LoginSchema),
     defaultValues: {
-      email: '',
-      password: '',
+      email: "",
+      password: "",
     },
-  })
-  const { signIn } = useAuthStore()
+  });
+  const { signIn } = useAuthStore();
 
-  const onSubmit = (values: LoginDto) => {
-
-    console.log(values)
-  }
+  const onSubmit = async (values: LoginDto) => {
+    const data = await signIn(values);
+    console.log(data);
+  };
   return (
     <form
       className="flex flex-col gap-6"
-      onSubmit={handleSubmit(onSubmit, err => console.log(err))}
+      onSubmit={handleSubmit(onSubmit, (err) => console.log(err))}
     >
       <label className="flex flex-col gap-2">
         Email
-        <Input id="email" {...register('email', { required: true })} />
+        <Input id="email" {...register("email", { required: true })} />
       </label>
       <label className="flex flex-col gap-2">
         Password
-        <Input id="password" {...register('password', { required: true })} />
+        <Input id="password" {...register("password", { required: true })} />
       </label>
       <button
         className=" w-full py-2 font-bold  border border-(--border) rounded-(--default-rounded) cursor-pointer hover:border-(--primary) hover:text-(--primary) "
--- store/auth-store.ts
+++ store/auth-store.ts
@@ -1,46 +1,45 @@
 import { apiManager } from "@/lib/api/api";
 import { LoginDto, User } from "@/types/user";
-import { create } from 'zustand';
-import { devtools, persist } from 'zustand/middleware';
-
-
-
+import { create } from "zustand";
+import { devtools, persist } from "zustand/middleware";
 
 interface AuthState {
-    user: User | null;
-    isAuthenticated: boolean;
-    isLoading: boolean;
-    error: string | null;
+  user: User | null;
+  isAuthenticated: boolean;
+  isLoading: boolean;
+  error: string | null;
 
-    signIn: (data: LoginDto) => Promise<void>;
-    signOut: () => Promise<void>;
-
+  signIn: (data: LoginDto) => Promise<void>;
+  signOut: () => Promise<void>;
 }
 
-
 export const useAuthStore = create<AuthState>()(
-    devtools(
-        persist(
-            (set, get) => ({
-                user: null,
-                isAuthenticated: false,
-                isLoading: false,
-                error: null,
-                signIn: async (data: LoginDto) => {
-                    set({ isLoading: true })
-                    try {
-                        const response = await apiManager.post('/auth/login', {
-                            json: data
-                        })
-
-                    } catch (error) {
-                        set({ isLoading: false })
-                    }
-                },
-                signOut: async () => {
-
-                }
-            }), { name: 'auth-store' }
-        )
-    )
-)
+  devtools(
+    persist(
+      (set, get) => ({
+        user: null,
+        isAuthenticated: false,
+        isLoading: false,
+        error: null,
+        signIn: async (data: LoginDto) => {
+          set({ isLoading: true });
+          try {
+            console.log("zustand");
+            console.log(data);
+            const response = await apiManager
+              .post("auth/login", {
+                json: data,
+              })
+              .json();
+            console.log(response);
+          } catch (error) {
+            console.log(error);
+            set({ isLoading: false });
+          }
+        },
+        signOut: async () => {},
+      }),
+      { name: "auth-store" },
+    ),
+  ),
+);
